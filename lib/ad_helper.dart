5a4187d137abe06ccb73d67418090203baaffdf0
Athar Wani <83573286+WaniAthar@users.noreply.github.com>
2022-05-05 20:28:35 +0530

    Update ad_helper.dart

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -3,8 +3,7 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId1 {
     if (Platform.isAndroid) {
-      // return "ca-app-pub-9854745246269551~5481703410";
-      return "ca-app-pub-4513986194145929/7824472321";
+       return "ca-app-pub-9854745246269551~5481703410";
       // } else if (Platform.isIOS) {
       //   return "ca-app-pub-9854745246269551/8539999649";
     } else {
@@ -14,8 +13,7 @@ class AdHelper {
 
   static String get fullPageAd {
     if (Platform.isAndroid) {
-      return "ca-app-pub-4513986194145929/4432022228";
-      // return "ca-app-pub-3940256099942544/1033173712";   //test ad
+      return "ca-app-pub-3940256099942544/1033173712";   //test ad
       // } else if (Platform.isIOS) {
       //   return "your interstitial ad unit id for ios";
     } else {
@@ -36,7 +34,7 @@ class AdHelper {
   static String get bannerAdUnitId2 {
     if (Platform.isAndroid) {
       // return "ca-app-pub-9854745246269551~5481703410";
-      return "ca-app-pub-4513986194145929/2544225484";
+      
       // } else if (Platform.isIOS) {
       //   return "ca-app-pub-9854745246269551/8539999649";
     } else {
@@ -47,7 +45,6 @@ class AdHelper {
   static String get bannerAdUnitId3 {
     if (Platform.isAndroid) {
       // return "ca-app-pub-9854745246269551~5481703410";
-      return "ca-app-pub-4513986194145929/4787245449";
       // } else if (Platform.isIOS) {
       //   return "ca-app-pub-9854745246269551/8539999649";
     } else {
@@ -58,7 +55,6 @@ class AdHelper {
   static String get bannerAdUnitId4 {
     if (Platform.isAndroid) {
       // return "ca-app-pub-9854745246269551~5481703410";
-      return "ca-app-pub-4513986194145929/8343347077";
       // } else if (Platform.isIOS) {
       //   return "ca-app-pub-9854745246269551/8539999649";
     } else {
@@ -69,7 +65,6 @@ class AdHelper {
   static String get bannerAdUnitId5 {
     if (Platform.isAndroid) {
       // return "ca-app-pub-9854745246269551~5481703410";
-      return "ca-app-pub-4513986194145929/5960726319";
       // } else if (Platform.isIOS) {
       //   return "ca-app-pub-9854745246269551/8539999649";
     } else {
@@ -80,7 +75,6 @@ class AdHelper {
   static String get bannerAdUnitId6 {
     if (Platform.isAndroid) {
       // return "ca-app-pub-9854745246269551~5481703410";
-      return "ca-app-pub-4513986194145929/4456072953";
       // } else if (Platform.isIOS) {
       //   return "ca-app-pub-9854745246269551/8539999649";
     } else {
@@ -91,7 +85,6 @@ class AdHelper {
   static String get bannerAdUnitId7 {
     if (Platform.isAndroid) {
       // return "ca-app-pub-9854745246269551~5481703410";
-      return "ca-app-pub-4513986194145929/9516827949";
       // } else if (Platform.isIOS) {
       //   return "ca-app-pub-9854745246269551/8539999649";
     } else {
@@ -102,7 +95,6 @@ class AdHelper {
   static String get bannerAdUnitId8 {
     if (Platform.isAndroid) {
       // return "ca-app-pub-9854745246269551~5481703410";
-      return "ca-app-pub-4513986194145929/9131182119";
       // } else if (Platform.isIOS) {
       //   return "ca-app-pub-9854745246269551/8539999649";
     } else {
@@ -113,7 +105,6 @@ class AdHelper {
   static String get bannerAdUnitId9 {
     if (Platform.isAndroid) {
       // return "ca-app-pub-9854745246269551~5481703410";
-      return "ca-app-pub-4513986194145929/4072929570";
       // } else if (Platform.isIOS) {
       //   return "ca-app-pub-9854745246269551/8539999649";
     } else {
@@ -123,7 +114,6 @@ class AdHelper {
 
   static String get webViewBanner1 {
     if (Platform.isAndroid) {
-      return "ca-app-pub-4513986194145929/6921071303";
       // } else if (Platform.isIOS) {
       //   return "your interstitial ad unit id for ios";
     } else {
